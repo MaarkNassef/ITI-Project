6dd38f9c29afbff276003e4106f31890d03d0711
George Samy <georgesamy668@gmail.com>
2022-08-06 00:45:53 +0200

    Search is added!!!

--- app.py
+++ app.py
@@ -3,15 +3,17 @@ import sqlite3
 import db
 app = Flask(__name__)
 app.config['SECRET_KEY']='SK'
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
+    if request.method=='POST':
+        search=request.form['search']
+        return redirect(url_for('search',word=search))      
     connect = sqlite3.connect('Database.db')
     cur=connect.cursor()
     cur.execute("select * from Books")
     data=cur.fetchall()
     connect.close()
     return render_template('index.html',data=data)
-
 
 @app.route('/signUp',methods=['POST','GET'])
 def signUp():
@@ -29,8 +31,6 @@ def signUp():
         else:
             flash("Password doesn't match!!")  
             return render_template('SignUp.html') 
-
-        
     else:
         return render_template('SignUp.html')
 
@@ -59,3 +59,17 @@ def book(id):
     data=cur.fetchone()
     connect.close()
     return render_template('book.html', data=data)
+ 
+@app.route('/search/<word>',methods=['GET','POST'])
+def search(word):
+    if request.method=='POST':
+        search=request.form['search']
+        word=search
+    conn = sqlite3.connect('Database.db')
+    cur = conn.cursor()
+    query = f"SELECT * FROM Books WHERE LOWER(Title) LIKE LOWER('%{word}%') OR LOWER(Category) LIKE LOWER('%{word}%') OR LOWER(Author) LIKE LOWER('%{word}%');"
+    cur.execute(query)
+    rows = cur.fetchall()
+    conn.close()
+    return render_template('search.html',rows=rows)
+
